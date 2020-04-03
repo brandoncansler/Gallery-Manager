4c9d53daa304f3c6e353a18e976f0401506f2dc3
antihero <mrcansler@gmail.com>
2020-04-03 11:45:55 -0500

    api routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,6 +2,13 @@
 var path = require("path");
 
 module.exports = function (app) {
+    app.get("/gallery", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/gallery.html"));
+    });
+
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
 
     app.get("/upload", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/upload.html"));
