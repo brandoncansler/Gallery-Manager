6a4f0a98426d6735c9eee77523050ac45c5cc8b4
antihero <mrcansler@gmail.com>
2020-04-03 11:21:37 -0500

    fixed upload widget

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,1 +1,9 @@
 // html routes
+var path = require("path");
+
+module.exports = function (app) {
+
+    app.get("/upload", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/upload.html"));
+    });
+};
--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static directory
-app.use(express.static("app/public"));
+app.use(express.static("public"));
 
 // Routes
 // =============================================================
-require("./app/routes/api-routes.js")(app);
-require("./app/routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 // Starts the server to begin listening
 // =============================================================
