b972f5cbf81bc61f21161486191dc72d1d6e6425
antihero <mrcansler@gmail.com>
2020-04-03 00:26:45 -0500

    view js

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -1,3 +1,38 @@
-// $(document).ready(function() {
+$(document).ready(function () {
+    // reference to input field
+    var $newGalleryInput = $("input.new-gallery");
 
-// });
+    // reference to gallery list
+    var $galleryList = $(".gallery-list");
+
+    // event listeners
+    $(document).on("submit", "#gallery-form", insertGallery);
+
+    // galleries array
+    var galleries = [];
+
+    // getting galleries from database on page load
+    getGalleries();
+
+    // getGalleries function
+    function getGalleries() {
+        $.get("/api/all", function (data) {
+            galleries = data;
+            initializeRows();
+        });
+    }
+
+    // function to construct new list item
+
+    // insert gallery
+    function insertGallery(event) {
+        event.preventDefault();
+        var gallery = {
+            text: $newGalleryInput.val().trim(),
+            complete: false
+        };
+
+        $.post("/api/all", gallery, getGalleries);
+        $newGalleryInput.val("");
+    }
+});
